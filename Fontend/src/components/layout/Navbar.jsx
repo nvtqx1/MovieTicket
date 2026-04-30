86839ef79fa58aad30d227721fae0812e405715b
Bui Duc Trong <soicadz50@gmail.com>
2026-05-01 02:50:28 +0700

    Sửa lại navbar, thêm route đến movies page

--- Fontend/src/components/layout/Navbar.jsx
+++ Fontend/src/components/layout/Navbar.jsx
@@ -5,9 +5,9 @@ import { useAuthContext } from "../../context/AuthContext";
 import defaultAvatar from "../../assets/images/avatarDefault.jpeg";
 
 const navLinks = [
+    { name: "Home", href: "/" },
     { name: "Phim", href: "/movies" },
-    { name: "Rạp chiếu", href: "/cinemas" },
-    { name: "Ưu đãi", href: "/promotions" },
+
 ];
 
 export default function Navbar() {
@@ -98,7 +98,7 @@ export default function Navbar() {
                     </div>
 
                     {/* Bell */}
-                    <button className="hidden sm:block text-gray-400 hover:text-white p-1">
+                    <button className="hidden sm:block text-gray-400 hover:text-white p-1 cursor-pointer">
                         <Bell size={18} />
                     </button>
 
@@ -134,7 +134,7 @@ export default function Navbar() {
 
                                 <button
                                     onClick={handleLogout}
-                                    className="w-full text-left px-4 py-3 text-sm text-gray-300 hover:bg-red-600 flex items-center gap-2"
+                                    className="w-full text-left px-4 py-3 text-sm text-gray-300 hover:bg-red-600 flex items-center gap-2 cursor-pointer"
                                 >
                                     <LogOut size={16} /> Đăng xuất
                                 </button>
@@ -143,7 +143,7 @@ export default function Navbar() {
                     ) : (
                         <button
                             onClick={() => navigate("/login")}
-                            className="hidden sm:block px-4 py-1.5 bg-red-600 text-white text-[10px] font-bold rounded-full uppercase tracking-widest"
+                            className="hidden sm:block px-4 py-1.5 bg-red-600 text-white text-[10px] font-bold rounded-full uppercase tracking-widest cursor-pointer"
                         >
                             Đăng nhập
                         </button>
