6a5f816b4a1b4a6dd284f13e085ae0b7dc4e151d
19thMarch <nvtqx1@gmail.com>
2026-05-08 19:10:14 +0700

    .

--- Backend/src/main/java/com/ticketrush/backend/controller/QueueController.java
+++ Backend/src/main/java/com/ticketrush/backend/controller/QueueController.java
@@ -12,6 +12,9 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 import java.util.Map;
 
