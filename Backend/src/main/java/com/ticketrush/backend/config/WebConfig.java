e6f5f7dccd6cfe56cb4b7b559f2668458d66854c
19thMarch <nvtqx1@gmail.com>
2026-05-10 03:44:44 +0700

    Update WebConfig.java

--- Backend/src/main/java/com/ticketrush/backend/config/WebConfig.java
+++ Backend/src/main/java/com/ticketrush/backend/config/WebConfig.java
@@ -14,7 +14,9 @@ public class WebConfig implements WebMvcConfigurer {
                         "http://localhost:5173",
                         "http://localhost:5174",
                         "http://127.0.0.1:5173",
-                        "http://127.0.0.1:5174"
+                        "http://127.0.0.1:5174",
+                        "movie-ticket-admin-sigma.vercel.app",
+                        "https://movie-ticket-mauve.vercel.app/"
                 )
                 .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                 .allowedHeaders("Authorization", "Content-Type", "X-Requested-With")
